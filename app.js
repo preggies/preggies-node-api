d90d40ef36c43e2e0838ea45b1c5911d7441a58d
Oyemade Oyemaja <oyemade.oyemaja@globalaccelerex.com>
2020-04-05 23:04:19 +0100

    dev dependencies

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const globalErrorHandler = require("./controllers/errorController");
 const AppError = require("./utils/appError");
@@ -7,13 +11,25 @@ const AppError = require("./utils/appError");
 // Initialize app
 const app = express();
 
+// Implement CORS
+app.use(cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 // Logger
 app.use(morgan("dev"));
 
 // Body parser
 app.use(
   express.json({
-    limit: "10kb"
+    limit: "10kb",
   })
 );
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 // Close server if there is any uncaught exceptions
-process.on("uncaughtException", err => {
+process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
   console.log("Uncaught Exception ❌❌❌");
   process.exit(1);
@@ -19,7 +20,7 @@ const server = app.listen(port, () => {
 });
 
 // Close server if there is any unhandled rejection (promises)
-process.on("unhandledRejection", err => {
+process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("Unhandled Rejection ❌❌❌");
   server.close(() => {
