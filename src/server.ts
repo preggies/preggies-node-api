260f1855cea52522481fdc4a18d59c6b6de82890
Ehbraheem <ehbraheem@gmail.com>
2020-04-09 23:34:50 +0100

    Hook up service to app

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,23 @@ import helmet from 'helmet';
 import mongoSanitize from 'express-mongo-sanitize';
 import xss from 'xss-clean';
 import { Mongoose } from 'mongoose';
+import dbConnect, { schema } from './api/persistence/mongoose';
 
 import globalErrorHandler from './api/controllers/errors';
 import AppError from './utils/appError';
 import './env';
+import loadConfig from './config';
 
 import services from './api/services';
 
 // Initialize app
 const app = nanoexpress();
+
+const config = loadConfig('');
+
+app.db = dbConnect(config);
+app.schema = schema(app.db);
+const serve = services(app.schema);
 
 // Implement CORS
 app.use(cors());
@@ -32,7 +40,7 @@ app.use(morgan('dev'));
 
 // Services
 app.use((req, _, next) => {
-  req.locals.services = services(app.get('db'));
+  req.locals.services = serve;
   next();
 });
 
