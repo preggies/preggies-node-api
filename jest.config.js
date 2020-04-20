4cd2fcaaed8473f3a027deff604d047160fbcf51
Ehbraheem <ehbraheem@gmail.com>
2020-04-20 02:56:51 +0100

    Better CI config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleDirectories: ['node_modules'],
-  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
-  reporters: ['default' /* "jest-junit" */],
+  collectCoverageFrom: ['src/**/**/**/*.{ts,tsx,js,jsx}'],
+  reporters: ['default', 'jest-junit'],
   coverageReporters: ['html', 'text', 'text-summary', 'lcov'],
   coverageThreshold: {
     global: {
@@ -13,8 +13,8 @@ module.exports = {
   },
   verbose: true,
   setupFilesAfterEnv: [
-    "<rootDir>/test/utils/databaseCleaner.ts",
-    "<rootDir>/test/utils/matchers.ts"
+    '<rootDir>/test/utils/databaseCleaner.ts',
+    '<rootDir>/test/utils/matchers.ts',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
